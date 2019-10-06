74a199c0088b7681f46806fa055ff95985f6af80
betinhoow <robertonobre90@gmail.com>
2019-10-06 18:41:25 -0300

    Teacher example one way with bytes

---
+++ one-way-bytes/src/networkEcho_I/ServerStart.java
@@ -0,0 +1,54 @@
+package networkEcho_I;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerStart
+   {
+   private static final String module = "Server";
+
+   public static void main(String[] args)
+      {
+      boolean isRunning = true;
+
+      System.out.println(Info.getUniformTitle());
+      System.out.println(module + " running.");
+      System.out.println();
+
+      try (ServerSocket serverSocket = new ServerSocket(Info.listeningPort))
+         {
+         System.out.println("Server at " + InetAddress.getLocalHost() + ", listening TCP port " + serverSocket.getLocalPort());
+         System.out.println();
+         
+         int msgRead = 0;
+         Socket newClientSocket = serverSocket.accept();
+
+         while (isRunning)
+            {
+            InputStream inputStream = newClientSocket.getInputStream();
+            byte[] buffer = new byte[Info.maxPackageSise];
+            int bytesRead = inputStream.read(buffer, 0, buffer.length);
+
+            if (bytesRead >= 0)
+               {
+               String plainText = (new String(buffer, 0, bytesRead));
+               System.out.println(module + " read " + (++msgRead) + ": " + plainText + " (" + bytesRead + " bytes)");
+               isRunning = !plainText.startsWith(Info.shutDownCmd);
+               }
+            }
+         serverSocket.close();
+         }
+      catch (IOException exception)
+         {
+         System.out.println("Exception launched: " + exception.getMessage());
+         System.exit(1);
+         }
+
+      System.out.println();
+      System.out.println(Info.getUniformTitle());
+      System.out.println(module + " stopped.");
+      }
+   }
